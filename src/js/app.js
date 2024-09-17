4cf1144cde23e5c0b4bad7548ecf72149b6b1bae
Dan <stickbugdanny@gmail.com>
2024-09-17 20:17:39 +0300

    lab2 task 2, 3, 4 & 5 done

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ const userFile = require('./random-user-mock.js');
 user_array = [];
 processed_user_array = [];
 
-//Reformats data from random-user-mock.js into needed format
+//TASK1: Reformat data from random-user-mock.js into needed format
 function format_data(random, additional) {
 //console.log(rawData.name);
 
@@ -64,7 +64,7 @@ formattedUser.timezone = define(user.timezone);
 
 formattedUser.email = define(user.email);
 
-formattedUser.b_date = define(user.b_date);
+formattedUser.b_date = define(user.b_day);
 formattedUser.age = define(user.age);
 
 formattedUser.phone = define(user.phone);
@@ -86,7 +86,7 @@ formattedUser.note = create_note(user.note);
 processed_user_array = check_for_duplicates(user_array);
 
 //return the final array
-return processed_user_array
+return processed_user_array;
 }
 
 function define(data) {
@@ -180,7 +180,146 @@ return filtered_array;
 }
 
 
-//call function
+//validation check
+//processed_user_array.forEach((user => {console.log('All users are valid: ' + validate_user(processed_user_array))});
+
+
+//TASK2: Validate object
+function validate_users(user_array) {
+
+for (i = 0; i < user_array.length; i++) {
+user = user_array[i];
+//string fields
+console.log('User ' + i + ' name is valid: ' + validate_string_field(user.full_name, i) +
+';\nGender is valid: ' + validate_string_field(user.gender, i) +
+';\nNote is valid: ' + validate_string_field(user.note, i) +
+';\nState is valid: ' + validate_string_field(user.state, i) +
+';\nCity is valid: ' + validate_string_field(user.city, i) +
+';\nCountry is valid: ' + validate_string_field(user.country, i) +
+';\nAge is valid: ' + isNumber(user.age) +
+';\nPhone is valid: ' + isValidPhoneNumber(user.phone) + //phone number WHAT DO YOU MEAN BY COUNTRY. AM I SUPPOSED TO WRITE A MAP???
+';\nEmail is valid: ' + isValidEmail(user.email));
+}
+}
+
+function validate_string_field(data, index) {
+field_is_valid = (isString(data) && firstLetterUppercase(data));
+
+if (!isString(data) && !firstLetterUppercase(data)) {//case if totally invalid
+console.log('User ' + index + ' field DOES NOT FIT CRITERIA;');
+} else if (!isString(data)) {//not string
+console.log('User ' + index + ' field IS NOT STRING');
+} else if (!firstLetterUppercase(data)) {//1st letter not capitalized
+console.log('User ' + index + ' field FIRST LETTER NOT CAPITALIZED');
+} else {
+return field_is_valid;
+}
+return field_is_valid;
+}
+
+
+function isString(value) {
+return typeof value === 'string';
+}
+
+function firstLetterUppercase(value) {
+return /^[A-Z]/.test(value);
+}
+
+function isNumber(age){
+if (age === null) {
+console.log('Age is null');
+return false;
+}
+return  Number.isInteger(age);
+}
+
+function isValidPhoneNumber(phone) {
+
+   //regex to check valid phone number.
+    const pattern = /^(?:[0-9\-\\(\\)\\/.]\s?){6,15}[0-9]{1}$/;
+
+    if (phone === null) {
+    console.log('Phone is null');
+    return false;
+    }
+    //return true if the phone number matched the regex
+    return pattern.test(phone)
+}
+
+
+function isValidEmail(email) {
+    if (email === null) {
+        console.log('Email is null');
+        return false;
+    }
+  return email.includes('@');
+}
+
+//TASK 3: Filter objects by 4 parameters (&&)
+function parameter_filter(user_array, country, age, gender, favorite) {
+
+const filtered_users = user_array.filter(user => user.country === country
+&& user.age === age
+&& user.gender === gender
+&& user.favorite === favorite);
+
+console.log(filtered_users);
+
+return filtered_users;
+}
+
+//TASK 4: Sort objects by 4 parameters (||)
+function parameter_sort(user_array, sort_by_full_name, sort_by_age, sort_by_b_date, sort_by_country, ascending) {
+user_array.sort((a, b) => {
+
+if (sort_by_full_name) {
+if (a.full_name < b.full_name) return ascending ? -1 : 1;
+if (a.full_name > b.full_name) return ascending ? 1 : -1;
+}
+
+if (sort_by_age) {
+if (a.age < b.age) return ascending ? -1 : 1;
+if (a.age > b.age) return ascending ? 1 : -1;
+}
+
+if(sort_by_b_date) {
+if (new Date(a.b_date) < new Date(b.b_date)) return ascending ? -1 : 1;
+if (new Date(a.b_date) > new Date(b.b_date)) return ascending ? 1 : -1;
+}
+
+if(sort_by_country) {
+if (a.country < b.country) return ascending ? -1 : 1;
+if (a.country > b.country) return ascending ? 1 : -1;
+}
+
+//IF both identical return 0
+return 0;
+});
+
+return user_array;
+}
+
+
+//TASK 5: find object by parameter
+//name, note, age
+function find_user(user_array, field){
+
+const filtered_users = user_array.filter(user => user.name === field
+|| user.note === field
+|| user.age === field);
+if(filtered_users.length == 0) {
+console.log('No users found');
+return null;
+}
+return filtered_users[0];
+}
+
+//call functions
 console.log(format_data(userFile.randomUserMock, userFile.additionalUsers));
+validate_users(processed_user_array);
+parameter_filter(processed_user_array, 'Switzerland', 39, 'female', false);//since favorite is randomly generated it can appear or not appear
+console.log(parameter_sort(processed_user_array, true, false, false, false, true));
+console.log(find_user(processed_user_array, 'User is shy~'));
 
 console.log(testModules.hello);
