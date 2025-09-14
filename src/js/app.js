ca10d45269deeb07af81431139c9f9aa2de09a82
Syn1ak <o.syniak@ukma.edu.ua>
2025-09-14 22:48:40 +0300

    lab1 done

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,43 @@
-import testModules from './test-module.js';
-/** ******** Your code here! *********** */
+require('../css/app.css');
 
-console.log(testModules.hello);
+document.addEventListener('DOMContentLoaded', () => {
+  const addTeacherBtns = document.querySelectorAll('.btn[data-target="add-teacher-popup"]');
+  const teacherCards = document.querySelectorAll('.teacher-card');
+  const closeAddTeacherPopupBtn = document.getElementById('close-add-teacher-button');
+  const closeTeacherInfoPopupBtn = document.getElementById('close-teacher-info-button');
+  const addTeacherPopup = document.getElementById('add-teacher-popup');
+  const teacherInfoPopup = document.getElementById('teacher-info-popup');
+
+  function showAddTeacherPopup() {
+    addTeacherPopup.showModal();
+  }
+
+  function showTeacherInfoPopup() {
+    teacherInfoPopup.showModal();
+  }
+
+  function hideAddTeacherPopup() {
+    addTeacherPopup.close();
+  }
+
+  function hideTeacherInfoPopup() {
+    teacherInfoPopup.close();
+  }
+
+  addTeacherBtns.forEach((button) => {
+    button.addEventListener('click', showAddTeacherPopup);
+  });
+
+  teacherCards.forEach((card) => {
+    card.addEventListener('click', showTeacherInfoPopup);
+  });
+
+  closeAddTeacherPopupBtn.addEventListener('click', hideAddTeacherPopup);
+
+  closeTeacherInfoPopupBtn.addEventListener('click', hideTeacherInfoPopup);
+
+  const confirmAddBtn = document.getElementById('add-teacher-button');
+  confirmAddBtn.addEventListener('click', () => {
+    hideAddTeacherPopup();
+  });
+});
