d4d865f9b6733f81b1a744dffb409613a53ed07c
Syn1ak <o.syniak@ukma.edu.ua>
2025-09-28 22:13:26 +0300

    lab3 done

---
+++ frontend/src/js/utils/filterUsers.ts
@@ -0,0 +1,40 @@
+import {StoredUser} from '../types/FormattedUser';
+import {UserFilters} from '../types/UserFilters';
+
+export function filterUsers(users: StoredUser[], filters: UserFilters): StoredUser[] {
+  return users.filter((user) =>
+    Object.entries(filters).every(([key, value]) => {
+      const userValue = user[key];
+
+      if (value === undefined) {
+        return true;
+      }
+
+      if (key === 'age') {
+        if (typeof value === 'number') {
+          return userValue === value;
+        } else if (typeof value === 'object' && value !== null) {
+          const {min, max} = value as { min?: number; max?: number };
+
+          if (min !== undefined && max !== undefined) {
+            return userValue >= min && userValue <= max;
+          } else if (min !== undefined) {
+            return userValue >= min;
+          } else if (max !== undefined) {
+            return userValue <= max;
+          }
+        }
+      }
+
+      if (typeof value === 'string' && typeof userValue === 'string') {
+        return userValue.toLocaleLowerCase() === value.toLocaleLowerCase();
+      }
+
+      if (typeof value === 'boolean') {
+        return !!userValue;
+      }
+
+      return userValue === value;
+    }),
+  );
+}
